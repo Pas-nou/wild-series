b472147b16d3e970bc432ac36c987be6dfbe3574
Pas-nou <pascal.renou@live.fr>
2024-05-24 11:06:28 +0200

    add programs router

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -2,13 +2,33 @@ const express = require("express");
 
 const router = express.Router();
 
+//  sayWelcome Route
+
+const { sayWelcome } = require('../../controllers/sayActions');
+
+router.get('/', sayWelcome);
+
+//  browse Route
+
+const { browse } = require('../../controllers/programActions');
+
+router.get('/', browse);
+
 /* ************************************************************************* */
 // Import And Use Routers Here
 /* ************************************************************************* */
 
+//  Items router
+
 const itemsRouter = require("./items/router");
 
 router.use("/items", itemsRouter);
+
+//  Progams router
+
+const progamsRouter = require('./programs/router');
+
+router.use('/programs', progamsRouter);
 
 /* ************************************************************************* */
 
